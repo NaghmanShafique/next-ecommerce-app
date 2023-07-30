adc31c32a5ed4135ce5a4c4fe8ed32d25cd8ac28
NaghmanShafique <naghman.shafique@gmail.com>
2023-07-30 18:19:56 +0500

    url issue changes

--- src/app/female/page.tsx
+++ src/app/female/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 import BASE_PATH_FORAPI from '@/components/shared/BaseApiPath';
 
 async function fetchAllProducts() {
-    let res = await fetch("https://next-ecom-app.vercel.app/api/products?start=0&end=10")
-    //console.log("URL "+BASE_PATH_FORAPI)
-    //let res = await fetch(`${BASE_PATH_FORAPI}/api/products?start=0&end=11`)
+    //let res = await fetch("http://localhost:3000/api/products?start=0&end=10")
+    console.log("URL "+BASE_PATH_FORAPI)
+    let res = await fetch(`${BASE_PATH_FORAPI}/api/products?start=0&end=11`)
     if (!res.ok){
         throw new Error("Failed to Fetch Products data.")
     }
