97c8cc95014af3780660d4573f9d3df9979f75fc
NaghmanShafique <naghman.shafique@gmail.com>
2023-07-30 18:23:20 +0500

    Url Issue 3

--- src/app/female/page.tsx
+++ src/app/female/page.tsx
@@ -5,7 +5,7 @@ import BASE_PATH_FORAPI from '@/components/shared/BaseApiPath';
 async function fetchAllProducts() {
     //let res = await fetch("http://localhost:3000/api/products?start=0&end=10")
     console.log("URL "+BASE_PATH_FORAPI)
-    let res = await fetch(`${BASE_PATH_FORAPI}/api/products?start=0&end=11`)
+    let res = await fetch(`${BASE_PATH_FORAPI}/api/products?start=0&end=11&type=Female`)
     if (!res.ok){
         throw new Error("Failed to Fetch Products data.")
     }
